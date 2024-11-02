3a9075e82f5084e450d4095ce6e07b5b6154574e
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-11-03 00:00:02 +0200

    added apps: accounts and projects

--- taskForge/settings.py
+++ taskForge/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'taskForge.accounts.apps.AccountsConfig',
+    'taskForge.projects.apps.ProjectsConfig'
 ]
 
 MIDDLEWARE = [
